9b6b3953ab8c8a6de0bcc1ab09c07613a3eb628d
ozelos <99196325+ozelos@users.noreply.github.com>
2026-03-09 13:01:28 +0200

    Update ConferenceController.php

--- app/Http/Controllers/Admin/ConferenceController.php
+++ app/Http/Controllers/Admin/ConferenceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Conference;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ConferenceController extends Controller
