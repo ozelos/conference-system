48431d47a69b1464f88e2cbcc2c863aa0df5cf0f
ozelos <99196325+ozelos@users.noreply.github.com>
2026-03-09 12:26:29 +0200

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Conference;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class EmployeeController extends Controller
 {
-    //
+    /**
+     * Rodo visų konferencijų sąrašą darbuotojui
+     */
+    public function index(): View
+    {
+        $conferences = Conference::all();  // arba ::latest()->get() jei nori rūšiuoti
+
+        return view('employee.conferences.index', compact('conferences'));
+    }
+
+    /**
+     * Rodo vieną konferenciją + užsiregistravusius
+     */
+    public function show(Conference $conference): View
+    {
+        $conference->load('registrations.user');  // užkrauname registracijas ir vartotojus
+
+        return view('employee.conferences.show', compact('conference'));
+    }
 }
