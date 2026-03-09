2f67d961fd2638f986c8851f341e5aeddd7d927b
ozelos <99196325+ozelos@users.noreply.github.com>
2026-03-09 13:04:48 +0200

    Update Conference.php

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,18 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conference extends Model
 {
+    use HasFactory;
+
+    /**
+     * Laukai, kuriuos leidžiama masiškai priskirti (mass assignment)
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'lecturers',
+        'date',
+        'time',
+        'location',
+        'is_past',  // jei naudoji šį lauką
+    ];
+
+    // Jei turi ryšius (registrations), pridėk čia
     public function registrations()
     {
         return $this->hasMany(Registration::class);
-    }
-
-    public function users()
-    {
-        return $this->belongsToMany(User::class, 'registrations')
-            ->withPivot('registered_at');
     }
 }
